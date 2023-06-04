3ac93c39e6c7fba6c74546d4d35fdc98a72c74fc
Iasmim Rodrigues <iasmimrodrigues1301@gmail.com>
2023-06-04 02:13:56 -0300

    Identificação das bandeiras do cartão funcionando

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const cardBrand = ["63", "55", "41"];
+
 document.querySelector("#card-number-input").oninput = () => {
   document.querySelector("#card-number-box").innerText =
     document.querySelector("#card-number-input").value;
+
+  let numberCard = document.querySelector("#card-number-input").value;
+  let flagNumber = numberCard.substr(0, 2);
+
+  const found = cardBrand.find((element) => element == flagNumber);
+
+  if (found == 63) {
+    document.getElementById("flag-icon").style.background =
+      "var(--elo-icon) no-repeat center";
+  } else if (found == 55) {
+    document.getElementById("flag-icon").style.background =
+      "var(--master-icon) no-repeat center";
+  } else if (found == 41) {
+    document.getElementById("flag-icon").style.background =
+      "var(--visa-icon) no-repeat center";
+  } /* else if (found != 63 || found != 55 || found != 41) {
+        alert("Esse número está incorreto!")
+    }*/
 };
 
 document.querySelector("#name-input").oninput = () => {
